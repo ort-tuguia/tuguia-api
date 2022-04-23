f0a467446aa0700c67d150a66bd2267c8ba829ce
daichanmiyashiro <47321602+daichanmiyashiro@users.noreply.github.com>
2022-04-23 20:29:51 -0300

    add properties

--- src/main/kotlin/edu/ort/tuguia/core/activity/domain/Activity.kt
+++ src/main/kotlin/edu/ort/tuguia/core/activity/domain/Activity.kt
@@ -1,4 +1,18 @@
 package edu.ort.tuguia.core.activity.domain
 
-class Activity {
+class Activity(code : String, name : String, description : String,coordinate : String , price : Float) {
+
+    var code : String
+    var name : String
+    var description : String
+    var coordinate : String
+    var price : Float
+
+    init {
+        this.code = code
+        this.name = name
+        this.description = description
+        this.coordinate = coordinate
+        this.price = price
+    }
 }
