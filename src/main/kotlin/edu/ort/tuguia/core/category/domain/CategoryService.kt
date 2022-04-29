c57ad23c34a7021a678feb0a2a88a1d80adb5821
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-04-29 19:40:58 -0300

    add category service & impl

--- src/main/kotlin/edu/ort/tuguia/core/category/domain/CategoryService.kt
+++ src/main/kotlin/edu/ort/tuguia/core/category/domain/CategoryService.kt
@@ -1,4 +1,55 @@
 package edu.ort.tuguia.core.category.domain
 
-class CategoryService {
+import edu.ort.tuguia.tools.helpers.http.ApiException
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import java.util.UUID
+
+interface CategoryService {
+    fun createCategory(category: Category): Category
+    fun getCategoryById(id: String): Category
+    fun getAllCategories(): List<Category>
+    fun updateCategory(category: Category): Category
+    fun deleteCategoryById(id: String): Category
+}
+
+@Service
+class CategoryServiceImpl(private val categoryRepository: CategoryRepository): CategoryService {
+    override fun createCategory(category: Category): Category {
+        category.id = UUID.randomUUID().toString()
+        category.createdAt = LocalDateTime.now()
+        this.categoryRepository.saveCategory(category)
+
+        return category
+    }
+
+    override fun getCategoryById(id: String): Category {
+        return this.categoryRepository.getCategoryById(id)
+            ?: throw ApiException(HttpStatus.NOT_FOUND, "La categoría con id $id no existe")
+    }
+
+    override fun getAllCategories(): List<Category> {
+        return this.categoryRepository.getAllCategories()
+    }
+
+    override fun updateCategory(category: Category): Category {
+        val queryCategory = this.getCategoryById(category.id)
+
+        queryCategory.name = category.name
+        queryCategory.description = category.description
+        queryCategory.updatedAt = LocalDateTime.now()
+
+        this.categoryRepository.saveCategory(queryCategory)
+
+        return queryCategory
+    }
+
+    override fun deleteCategoryById(id: String): Category {
+        val queryCategory = this.getCategoryById(id)
+
+        this.categoryRepository.deleteCategory(queryCategory)
+
+        return queryCategory
+    }
 }
