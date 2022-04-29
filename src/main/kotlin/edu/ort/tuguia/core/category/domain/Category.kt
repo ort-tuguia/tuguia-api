d7f4694bed6f42271aa4213e2668d04f1dd1676b
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-04-29 19:39:48 -0300

    set category entity

--- src/main/kotlin/edu/ort/tuguia/core/category/domain/Category.kt
+++ src/main/kotlin/edu/ort/tuguia/core/category/domain/Category.kt
@@ -1,15 +1,30 @@
 package edu.ort.tuguia.core.category.domain
 
-class Category(categoryId : String, name : String, description : String) {
+import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+import javax.validation.constraints.NotBlank
 
-    var categoryId : String
-    var name : String
-    var description : String
+@Entity
+@Table(name = "categories")
+class Category(id: String = "", name: String = "", description: String = "") {
+    @Id
+    var id: String
+
+    @NotBlank
+    var name: String
+
+    @NotBlank
+    var description: String
+
+    lateinit var createdAt: LocalDateTime
+
+    var updatedAt: LocalDateTime? = null
 
     init {
-        this.categoryId = categoryId
+        this.id = id
         this.name = name
         this.description = description
     }
-
 }
