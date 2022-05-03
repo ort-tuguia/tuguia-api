cf3e04732f8ed319da767127476bb03a240ab086
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-05-02 21:28:11 -0300

    add get category by name impl

--- src/main/kotlin/edu/ort/tuguia/core/category/domain/CategoryService.kt
+++ src/main/kotlin/edu/ort/tuguia/core/category/domain/CategoryService.kt
@@ -9,6 +9,7 @@ import java.util.*
 interface CategoryService {
     fun createCategory(category: Category): Category
     fun getCategoryById(id: String): Category
+    fun getCategoryByName(name: String): Category
     fun getAllCategories(): List<Category>
     fun updateCategory(category: Category): Category
     fun deleteCategoryById(id: String): Category
@@ -17,6 +18,11 @@ interface CategoryService {
 @Service
 class CategoryServiceImpl(private val categoryRepository: CategoryRepository) : CategoryService {
     override fun createCategory(category: Category): Category {
+        val queryCategory = this.categoryRepository.getCategoryByName(category.name)
+        if (queryCategory != null) {
+            throw ApiException(HttpStatus.BAD_REQUEST, "La categoría con el nombre ${category.name} ya existe")
+        }
+
         category.id = UUID.randomUUID().toString()
         category.createdAt = LocalDateTime.now()
         this.categoryRepository.saveCategory(category)
@@ -27,6 +33,11 @@ class CategoryServiceImpl(private val categoryRepository: CategoryRepository) : 
     override fun getCategoryById(id: String): Category {
         return this.categoryRepository.getCategoryById(id)
             ?: throw ApiException(HttpStatus.NOT_FOUND, "La categoría con id $id no existe")
+    }
+
+    override fun getCategoryByName(name: String): Category {
+        return this.categoryRepository.getCategoryByName(name)
+            ?: throw ApiException(HttpStatus.NOT_FOUND, "La categoría con nombre $name no existe")
     }
 
     override fun getAllCategories(): List<Category> {
