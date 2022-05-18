dd72b1e13f32e1f75495e555084f56928db89474
alejandrogrosso <alematiasgrosso@gmail.com>
2022-05-18 16:31:39 -0300

    Update PhotoController.kt

--- src/main/kotlin/edu/ort/tuguia/core/photo/domain/PhotoController.kt
+++ src/main/kotlin/edu/ort/tuguia/core/photo/domain/PhotoController.kt
@@ -2,9 +2,11 @@ package edu.ort.tuguia.core.photo.domain
 
 
 import edu.ort.tuguia.core.category.domain.CategoryService
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.io.IOException
@@ -16,17 +18,19 @@ import java.io.IOException
 class PhotoController(private val photoService: PhotoService) {
 
     var logger: Logger = LoggerFactory.getLogger(PhotoController::class.java)
-
+    @Operation(summary = "Upload Photo")
     @PostMapping("/upload")
-    fun upload(@RequestParam("file") multipartFile: MultipartFile): Any? {
+    @ResponseStatus(HttpStatus.OK)
+    fun upload(@RequestParam("file") multipartFile: MultipartFile): Any {
         logger.info("HIT -/upload | File Name : {}", multipartFile.originalFilename)
         return photoService.upload(multipartFile)
     }
-
-//    @PostMapping("/profile/pic/{fileName}")
-//    @Throws(IOException::class)
-//    fun download(@PathVariable fileName: String?): Any? {
-//        logger.info("HIT -/download | File Name : {}", fileName)
-//        return photoService.download(fileName)
-//    }
+    @Operation(summary = "Download Photo")
+    @PostMapping("/{fileName}")
+    @ResponseStatus(HttpStatus.OK)
+    @Throws(IOException::class)
+    fun download(@PathVariable fileName: String): Any {
+        logger.info("HIT -/download | File Name : {}", fileName)
+        return photoService.download(fileName)
+    }
 }
