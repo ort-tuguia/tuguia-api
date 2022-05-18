f76026e5992c98f4d4f1b67bec87a5448f7d6f58
alejandrogrosso <alematiasgrosso@gmail.com>
2022-05-18 16:31:31 -0300

    Update Photo.kt

--- src/main/kotlin/edu/ort/tuguia/core/photo/domain/Photo.kt
+++ src/main/kotlin/edu/ort/tuguia/core/photo/domain/Photo.kt
@@ -1,17 +1,30 @@
 package edu.ort.tuguia.core.photo.domain
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
 import javax.validation.constraints.NotBlank
-
-class Photo(id: String, url: String) {
+@Entity
+@Table(name = "photos")
+class Photo(id: String = "", filename: String = "") {
     @Schema(readOnly = true)
+    @Id
     var id: String
 
-    @NotBlank(message = "La url es obligatoria")
-    var url: String
+    @NotBlank(message = "El nombre es obligatorio")
+    var filename: String
+    @Schema(readOnly = true)
+    lateinit var createdAt: LocalDateTime
+
+    @Schema(readOnly = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    var updatedAt: LocalDateTime? = null
 
     init {
         this.id = id
-        this.url = url
+        this.filename = filename
     }
 }
