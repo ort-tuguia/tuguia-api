b72edf66888a39e30802055af1ffac41234e3ba1
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-06-04 17:17:30 -0300

    set env

--- src/main/kotlin/edu/ort/tuguia/core/activity/repositories/ActivityOrionRepository.kt
+++ src/main/kotlin/edu/ort/tuguia/core/activity/repositories/ActivityOrionRepository.kt
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Repository
 
 @Repository
-@Profile("production|default")
+@Profile("production")
 class ActivityOrionRepository(val orionClient: OrionClient) : ActivityRepository {
     val entityType = "Activity"
 
