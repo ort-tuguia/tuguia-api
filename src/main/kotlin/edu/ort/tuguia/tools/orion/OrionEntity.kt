9d17fd248d4cac1fadae6c862f2b9949f67710f7
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-06-04 16:59:12 -0300

    add orion entity builder, attr and types

--- src/main/kotlin/edu/ort/tuguia/tools/orion/OrionEntity.kt
+++ src/main/kotlin/edu/ort/tuguia/tools/orion/OrionEntity.kt
@@ -1,17 +1,23 @@
 package edu.ort.tuguia.tools.orion
 
-data class OrionAttr(val attr: String, val type: String, val value: Any)
-
 class OrionEntity {
+    class Attr<T>(val name: String = "", val type: String = "", val value: T)
+
     companion object {
-        fun orionEntityBuilder(id: String, type: String, values: List<OrionAttr>): Map<String, Any> {
+
+        const val TypeString = "String"
+        const val TypeDouble = "Double"
+        const val TypeTimestamp = "Timestamp"
+        const val TypeGeoPoint = "geo:point"
+
+        fun orionEntityBuilder(id: String, type: String, values: List<Attr<Any>>): Map<String, Any> {
             val entity = mutableMapOf<String, Any>(
                 "id" to id,
                 "type" to type
             )
 
             values.forEach {
-                entity[it.attr] = mapOf(
+                entity[it.name] = mapOf(
                     "type" to it.type,
                     "value" to it.value
                 )
