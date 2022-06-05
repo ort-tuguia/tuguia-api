37a6a7ceab8e24a97795f3ead265d4c1b737df99
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-06-05 16:57:44 -0300

    add validations in activity search options

--- src/main/kotlin/edu/ort/tuguia/core/activity/domain/ActivitySearchOptions.kt
+++ src/main/kotlin/edu/ort/tuguia/core/activity/domain/ActivitySearchOptions.kt
@@ -1,9 +1,32 @@
 package edu.ort.tuguia.core.activity.domain
 
-data class ActivitySearchOptions(
-    val currentLatitude: Double,
-    val currentLongitude: Double,
-    val maxKm: Double,
-    val categoriesIds: List<String>,
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+
+class ActivitySearchOptions(
+    currentLatitude: Double,
+    currentLongitude: Double,
+    maxKm: Double,
+    categoriesIds: List<String>,
+    maxResults: Int
+) {
+    val currentLatitude: Double
+
+    val currentLongitude: Double
+
+    @Min(1, message = "El radio mínimo es 1 Km")
+    @Max(1000, message = "El radio máximo es 1000 Km")
+    val maxKm: Double
+
+    val categoriesIds: List<String>
+
     val maxResults: Int
-)
+
+    init {
+        this.currentLatitude = currentLatitude
+        this.currentLongitude = currentLongitude
+        this.maxKm = maxKm
+        this.categoriesIds = categoriesIds
+        this.maxResults = maxResults
+    }
+}
