fb5fe9428a1e1cf88f468ec6a69936ef4e34a0a7
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-06-04 21:28:39 -0300

    add login, logout and get logged user details

--- src/main/kotlin/edu/ort/tuguia/core/user/domain/UserController.kt
+++ src/main/kotlin/edu/ort/tuguia/core/user/domain/UserController.kt
@@ -1,10 +1,13 @@
 package edu.ort.tuguia.core.user.domain
 
+import edu.ort.tuguia.tools.auth.JwtAuth
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 import javax.validation.Valid
 
 @Tag(name = "Users")
@@ -21,7 +24,24 @@ class UserController(private val userService: UserService) {
     @Operation(summary = "Login")
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    fun loginUser(@RequestBody @Valid @Parameter(description = "Login data") login: Login): User? {
+    fun loginUser(@RequestBody @Valid @Parameter(description = "Login data") login: Login, response: HttpServletResponse): User? {
+        val user = this.userService.loginUser(login)
+        JwtAuth.initializeToken(user.username, response)
         return this.userService.loginUser(login)
+    }
+
+    @Operation(summary = "Logout")
+    @PostMapping("/logout")
+    @ResponseStatus(HttpStatus.OK)
+    fun logoutUser(response: HttpServletResponse) {
+        JwtAuth.clearToken(response)
+    }
+
+    @Operation(summary = "Get User Details")
+    @PostMapping("/details")
+    @ResponseStatus(HttpStatus.OK)
+    fun getUserDetails(request: HttpServletRequest): User? {
+        val username = JwtAuth.getUsernameFromRequest(request)
+        return this.userService.getUserByUsername(username)
     }
 }
