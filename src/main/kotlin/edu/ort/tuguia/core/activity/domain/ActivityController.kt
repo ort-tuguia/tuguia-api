bd35f4e9031b0b9f8e4493c0cd50641e00205445
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-06-07 17:43:52 -0300

    inject in create activity

--- src/main/kotlin/edu/ort/tuguia/core/activity/domain/ActivityController.kt
+++ src/main/kotlin/edu/ort/tuguia/core/activity/domain/ActivityController.kt
@@ -1,10 +1,12 @@
 package edu.ort.tuguia.core.activity.domain
 
+import edu.ort.tuguia.tools.auth.JwtAuth
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpServletRequest
 import javax.validation.Valid
 
 @Tag(name = "Activities")
@@ -14,8 +16,12 @@ class ActivityController(private val activityService: ActivityService) {
     @Operation(summary = "Create an activity")
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
-    fun createActivity(@RequestBody @Valid @Parameter(description = "Activity") activity: Activity): Activity? {
-        return this.activityService.createActivity(activity)
+    fun createActivity(
+        request: HttpServletRequest,
+        @RequestBody @Valid @Parameter(description = "Activity") activity: Activity
+    ): Activity? {
+        val username = JwtAuth.getUsernameFromRequest(request)
+        return this.activityService.createActivity(username, activity)
     }
 
     @Operation(summary = "Get activity by ID")
