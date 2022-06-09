37e8dcf295eb719208ee822768d3266207930ac7
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-06-08 23:41:44 -0300

    add user photo in user domain

--- src/main/kotlin/edu/ort/tuguia/core/user/domain/UserPhone.kt
+++ src/main/kotlin/edu/ort/tuguia/core/user/domain/UserPhone.kt
@@ -1,15 +1,17 @@
-package edu.ort.tuguia.core.phone.domain
+package edu.ort.tuguia.core.user.domain
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import edu.ort.tuguia.core.user.domain.User
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
-import javax.persistence.*
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
 import javax.validation.constraints.NotBlank
 
 @Entity
-@Table(name = "phones")
-class Phone(
+@Table(name = "users_phones")
+class UserPhone(
     id: String = "",
     number: String = "",
     description: String = "",
@@ -19,22 +21,18 @@ class Phone(
     @Id
     var id: String
 
-    @NotBlank(message = "El numero es obligatorio")
+    @NotBlank(message = "El número es obligatorio")
     var number: String
 
-    @NotBlank(message = "La descripcion es obligatoria")
+    @NotBlank(message = "La descripción es obligatoria")
     var description: String
 
     @ManyToOne
-    @JoinColumn(name = "username", nullable =  false)
+    @JoinColumn(name = "username", nullable = false)
     private var user: User?
 
     @Schema(readOnly = true)
     var createdAt: LocalDateTime
-
-    @Schema(readOnly = true)
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    var updatedAt: LocalDateTime? = null
 
     init {
         this.id = id
