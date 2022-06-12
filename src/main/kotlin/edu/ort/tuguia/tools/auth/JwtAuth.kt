d3a475ca900baa77919b885db03836f6ad7f754c
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-06-12 17:47:50 -0300

    inject role in jwt token

--- src/main/kotlin/edu/ort/tuguia/tools/auth/JwtAuth.kt
+++ src/main/kotlin/edu/ort/tuguia/tools/auth/JwtAuth.kt
@@ -1,6 +1,7 @@
 package edu.ort.tuguia.tools.auth
 
 import edu.ort.tuguia.tools.helpers.http.ApiException
+import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.http.HttpStatus
@@ -10,15 +11,29 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 private const val CookieKey = "jwt"
+private const val RoleKey = "role"
 private const val SecretKey = "secret"
 private const val AuthHeader = "Authorization"
 private const val TokenPrefix = "Bearer "
 
 class JwtAuth {
+    class LoggedUser(val username: String, val role: String) {
+        fun hasRole(role: String): Boolean {
+            return this.role == role
+        }
+        fun hasAnyRole(roles: List<String>): Boolean {
+            roles.forEach {
+                if (this.role == it) return true
+            }
+            return false
+        }
+    }
+
     companion object {
-        fun initializeToken(username: String, response: HttpServletResponse) {
+        fun initializeToken(username: String, role: String, response: HttpServletResponse) {
             val jwtToken = Jwts.builder()
                 .setIssuer(username)
+                .claim(RoleKey, role)
                 .setExpiration(Date(System.currentTimeMillis() + 60 * 24 * 1000)) // 1 day
                 .signWith(SignatureAlgorithm.HS256, SecretKey).compact()
 
@@ -32,10 +47,10 @@ class JwtAuth {
             response.addCookie(jwtCookie)
         }
 
-        fun getUsernameFromRequest(request: HttpServletRequest): String {
+        fun getUserFromRequest(request: HttpServletRequest): LoggedUser {
             val jwtToken = this.getTokenFromRequest(request)
                 ?: throw ApiException(HttpStatus.UNAUTHORIZED, "User not found in context")
-            return this.getUsernameFromToken(jwtToken)
+            return this.getUserFromToken(jwtToken)
         }
 
         fun getTokenFromRequest(request: HttpServletRequest): String? {
@@ -70,10 +85,15 @@ class JwtAuth {
             return authHeader.removePrefix(TokenPrefix)
         }
 
-        private fun getUsernameFromToken(jwtToken: String): String {
-            return Jwts.parser()
-                .setSigningKey(SecretKey)
-                .parseClaimsJws(jwtToken).body.issuer
+        private fun getUserFromToken(jwtToken: String): LoggedUser {
+            try {
+                val body = Jwts.parser()
+                    .setSigningKey(SecretKey)
+                    .parseClaimsJws(jwtToken).body
+                return LoggedUser(body.issuer, body[RoleKey] as String)
+            } catch (e: ExpiredJwtException) {
+                throw ApiException(HttpStatus.BAD_REQUEST, "Sesión expirada. Se debe realizar login nuevamente")
+            }
         }
     }
 }
