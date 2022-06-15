3a4654b1dd815aee79194eaa9ae489aac34af61a
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-06-15 19:07:19 -0300

    delete old review entity

--- src/main/kotlin/edu/ort/tuguia/core/review/domain/ReviewRepository.kt
+++
@@ -1,8 +0,0 @@
-package edu.ort.tuguia.core.review.domain
-
-interface ReviewRepository {
-    fun saveReview(review: Review)
-    fun getReviewById(id: String): Review?
-    fun getAllReviews(): List<Review>
-    fun deleteReview(review: Review)
-}
