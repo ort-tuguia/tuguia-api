e5b61993d5ca20af0d579a8869e9b48821bc04b7
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-06-15 22:46:11 -0300

    update cors config

--- src/main/kotlin/edu/ort/tuguia/middleware/CorsMiddleware.kt
+++ src/main/kotlin/edu/ort/tuguia/middleware/CorsMiddleware.kt
@@ -11,7 +11,9 @@ class CorsMiddleware : HandlerInterceptor {
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         if (request.method == "OPTIONS") {
             response.setHeader("Access-Control-Allow-Origin", "*")
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
             response.setHeader("Access-Control-Max-Age", AccessControlMaxAge)
+            response.setHeader("Access-Control-Allow-Headers", "authorization, content-type")
             response.status = HttpStatus.OK.value()
 
             return false
