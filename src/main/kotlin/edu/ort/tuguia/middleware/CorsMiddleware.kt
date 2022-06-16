5df0502bc0a6ea55eaaba19354ce6b91df96e12b
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-06-15 22:40:07 -0300

    add cors middleware config

--- src/main/kotlin/edu/ort/tuguia/middleware/CorsMiddleware.kt
+++ src/main/kotlin/edu/ort/tuguia/middleware/CorsMiddleware.kt
@@ -1,14 +1,19 @@
 package edu.ort.tuguia.middleware
 
-import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.web.servlet.HandlerInterceptor
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+private const val AccessControlMaxAge = "300"
+
 class CorsMiddleware : HandlerInterceptor {
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        if (request.method == HttpMethod.OPTIONS.name) {
+        if (request.method == "OPTIONS") {
+            response.setHeader("Access-Control-Allow-Origin", "*")
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+            response.setHeader("Access-Control-Max-Age", AccessControlMaxAge)
+            response.setHeader("Access-Control-Allow-Headers", "authorization, content-type")
             response.status = HttpStatus.OK.value()
 
             return false
