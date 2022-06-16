7a01795ed555f134d75175049446b6f717634cbd
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-06-15 22:13:07 -0300

    update cors middleware config

--- src/main/kotlin/edu/ort/tuguia/middleware/CorsMiddleware.kt
+++ src/main/kotlin/edu/ort/tuguia/middleware/CorsMiddleware.kt
@@ -6,13 +6,9 @@ import org.springframework.web.servlet.HandlerInterceptor
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-private const val AccessControlMaxAgeValue = "300"
-
 class CorsMiddleware : HandlerInterceptor {
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         if (request.method == HttpMethod.OPTIONS.name) {
-            response.addHeader("Access-Control-Allow-Origin", "*")
-            response.addHeader("Access-Control-Max-Age", AccessControlMaxAgeValue)
             response.status = HttpStatus.OK.value()
 
             return false
