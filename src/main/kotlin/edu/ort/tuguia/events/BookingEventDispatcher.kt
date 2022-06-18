98599502ff5d2f5e70461fefc32074cda74379c3
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2022-06-18 20:00:07 -0300

    add async update use guide reviews event

--- src/main/kotlin/edu/ort/tuguia/events/BookingEventDispatcher.kt
+++ src/main/kotlin/edu/ort/tuguia/events/BookingEventDispatcher.kt
@@ -2,14 +2,17 @@ package edu.ort.tuguia.events
 
 import edu.ort.tuguia.core.activity.domain.ActivityService
 import edu.ort.tuguia.core.booking.domain.BookingEvent
+import edu.ort.tuguia.core.user.domain.UserService
 import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Component
 
 @Component
 class BookingEventDispatcher(
-    private val activityService: ActivityService
+    private val activityService: ActivityService,
+    private val userService: UserService
 ) : ApplicationListener<BookingEvent> {
     override fun onApplicationEvent(event: BookingEvent) {
         this.activityService.asyncUpdateActivityReviews(event.booking.getActivityId())
+        this.userService.asyncUpdateUserReviews(event.booking.getGuideUsername())
     }
 }
--- src/main/kotlin/edu/ort/tuguia/events/ReviewEventDispatcher.kt
+++ src/main/kotlin/edu/ort/tuguia/events/ReviewEventDispatcher.kt
@@ -2,14 +2,17 @@ package edu.ort.tuguia.events
 
 import edu.ort.tuguia.core.activity.domain.ActivityService
 import edu.ort.tuguia.core.review.domain.ReviewEvent
+import edu.ort.tuguia.core.user.domain.UserService
 import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Component
 
 @Component
 class ReviewEventDispatcher(
-    private val activityService: ActivityService
+    private val activityService: ActivityService,
+    private val userService: UserService
 ) : ApplicationListener<ReviewEvent> {
     override fun onApplicationEvent(event: ReviewEvent) {
         this.activityService.asyncUpdateActivityReviews(event.review.getActivityId())
+        this.userService.asyncUpdateUserReviews(event.review.getGuideUsername())
     }
 }
